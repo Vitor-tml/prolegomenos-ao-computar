faf583f6ffcd2eb22a98518af8354ce3d86f45cb
Vitor Silva <vitor.gabriel.vinielle@gmail.com>
2022-12-14 00:30:32 -0300

    trabalho finalizado

--- trabalho-final/projeto/projeto.ino
+++ trabalho-final/projeto/projeto.ino
@@ -16,17 +16,27 @@
 
 // Definindo saida do buzzer
 #define BUZZ 3
-// Tefine tons
-#define LA 440
+
+// Define tons
+#define DO 130
+#define RE 146
+#define MI 164
+#define FA 174
+#define LA 220
+
 // Quantidade de LEDs e botões
 #define TAM 4
 
 // Variáveis globais
 int leds[TAM] = {RED, YELLOW, GREEN, BLUE};
 int botoes[TAM] = {BRED, BYELLOW, BGREEN, BBLUE};
-
+int notas[TAM] = {DO, RE, MI, FA};
+int rodada = 0;
 Adafruit_LiquidCrystal lcd_1(0);
+// Protótipo das funções
 int botaoApertado();
+int testaSequencia(int *resposta);
+void mostraSequencia(int *sequencia, int rodada);
 
 void setup()
 {
@@ -40,12 +50,72 @@ void setup()
     pinMode(botoes[i], INPUT);
   }
   pinMode(BUZZ, OUTPUT);
+  randomSeed(analogRead(0));
 }
 
 void loop()
 {
+  int tentativa[TAM];
+  int resposta[TAM];
+  int i;
+  lcd_1.clear();
+  // Gera sequencia aleatoria
+  for(i = 0; i < TAM; i++)
+  {
+    resposta[i] = random(0, TAM);
+  }
+  mostraSequencia(resposta, rodada);
+  if(testaSequencia(resposta))
+  {
+    lcd_1.print("Correto!");
+    rodada++;
+  }
+  else
+  {
+    lcd_1.print("Errado!");
+    derrota();
+    delay(500);
+    rodada = 0;
+  }
+  if(rodada == 3)
+  {
+    vitoria();
+    rodada = 0;
+    lcd_1.print("Voce venceu!!!");
+  }
+  
+  delay(700);
+  
+}
+// Recebe sequência e lê resposta, retorna 1 caso a resposta for correta
+int testaSequencia(int *resposta)
+{
+  int i;
+  for(i = 0; i < TAM; i++)
+  {
+    if(resposta[i] != botaoApertado())
+    {
+      return 0;
+    }
+  }
+  return 1;
 }
 
+// Recebe sequência resposta e acende os LEDs nessa sequência
+void mostraSequencia(int *sequencia, int rodada)
+{
+  int i;
+  
+  for(i = 0; i < TAM; i++)
+  {
+    digitalWrite(leds[sequencia[i]], HIGH);
+    tone(BUZZ, notas[sequencia[i]], 200);
+    delay(500 - (100 * rodada));
+    digitalWrite(leds[sequencia[i]], LOW);
+  }
+}
+
+// Fica checando os botões em loop, retorna o primeiro botão apertado
 int botaoApertado()
 {
   int i = 0;
@@ -58,12 +128,31 @@ int botaoApertado()
       {
         while(digitalRead(botoes[i]) == HIGH)
         {
-          tone(BUZZ, LA, 100);
+          tone(BUZZ, notas[i], 100);
+          digitalWrite(leds[i], HIGH);
         }
-        
+        digitalWrite(leds[i], LOW);
         return i;
       }
     }
   }
+}
 
+// Som da derrota
+void derrota()
+{
+  tone(BUZZ, 80, 1500);
+  delay(1500);
+}
+void vitoria()
+{
+  tone(BUZZ, 293, 300);
+  delay(350);
+  tone(BUZZ, 293, 300);
+  delay(350);
+  tone(BUZZ, 329, 400);
+  delay(450);
+  tone(BUZZ, 293, 550);
+  delay(600);
+  
 }
